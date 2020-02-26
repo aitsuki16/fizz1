24401c9e33559ad50aac7be8da7254ce29e0107c
USER <>
2020-02-26 12:15:26 +0900

    finished

---
+++ FizzBuzz1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzz1
+//
+//  Created by USER on 2020/02/26.
+//  Copyright © 2020 Igarashi Ai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
